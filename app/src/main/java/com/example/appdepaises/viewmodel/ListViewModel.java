229e00a270cbeca07c0f44f258a0d0cfffe0d841
Igor Montezuma <85948415+IgorMontezuma20@users.noreply.github.com>
2022-09-22 22:24:16 -0300

    implementando a ViewModel

--- app/src/main/java/com/example/appdepaises/viewmodel/ListViewModel.java
+++ app/src/main/java/com/example/appdepaises/viewmodel/ListViewModel.java
@@ -1,4 +1,35 @@
 package com.example.appdepaises.viewmodel;
 
-public class ListViewModel {
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+import com.example.appdepaises.model.CountryModel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListViewModel extends ViewModel {
+
+    public MutableLiveData<List<CountryModel>> countries = new MutableLiveData<List<CountryModel>>();
+    public MutableLiveData<Boolean> CountryLoadError = new MutableLiveData<Boolean>();
+    public MutableLiveData<Boolean> loading = new MutableLiveData<Boolean>();
+
+    public void refresh(){
+        fetchCountries();
+    }
+
+    private void fetchCountries(){
+        CountryModel country1 = new CountryModel("Albania", "Tirana", "");
+        CountryModel country2 = new CountryModel("Brazil", "Brasilia", "");
+        CountryModel country3 = new CountryModel("Czechia", "Praha", "");
+
+        List<CountryModel> list = new ArrayList<>();
+        list.add(country1);
+        list.add(country2);
+        list.add(country3);
+
+        countries.setValue(list);
+        CountryLoadError.setValue(false);
+        loading.setValue(false);
+    }
 }
