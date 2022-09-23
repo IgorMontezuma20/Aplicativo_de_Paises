5c8383b450907a0f6fd36f0e1882d36e9fbc1116
Igor Montezuma <85948415+IgorMontezuma20@users.noreply.github.com>
2022-09-22 21:56:19 -0300

    configurando a CountryModel

--- app/src/main/java/com/example/appdepaises/model/CountryModel.java
+++ app/src/main/java/com/example/appdepaises/model/CountryModel.java
@@ -1,4 +1,26 @@
 package com.example.appdepaises.model;
 
 public class CountryModel {
+
+    String countryName;
+    String capital;
+    String flag;
+
+    public CountryModel(String countryName, String capital, String flag) {
+        this.countryName = countryName;
+        this.capital = capital;
+        this.flag = flag;
+    }
+
+    public String getCountryName() {
+        return countryName;
+    }
+
+    public String getCapital() {
+        return capital;
+    }
+
+    public String getFlag() {
+        return flag;
+    }
 }
